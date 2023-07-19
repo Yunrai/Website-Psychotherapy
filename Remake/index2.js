ceb752e4452c13ed656232ef3031b3205b7f14c8
Yunrai <darksh13@gmail.com>
2023-07-19 17:00:21 +0300

    remade the calendar and added the JS for it

--- Remake/index2.js
+++ Remake/index2.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const toggle = document.querySelector('#togglebtn');
 const navLi = document.querySelector('#navList');
 
@@ -18,5 +20,96 @@ window.addEventListener('resize', () => {
         navLi.style.display = "none";
    else 
      navLi.style.display = "block";
-})
+});
 
+ //booking calendar
+
+window.addEventListener("load", function startTime() {
+     const today = new Date();
+     let h = today.getHours();
+     let m = today.getMinutes();
+     let s = today.getSeconds();
+     m = checkTime(m);
+     s = checkTime(s);
+     document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+     setTimeout(startTime, 1000);
+}, false);
+   
+function checkTime(i) {
+     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+     return i;
+};
+   
+window.addEventListener("load", function currentDate(){
+     const today = new Date();
+     let day = today.getDate();
+     let month = today.getMonth() + 1;
+     let year = today.getFullYear();
+     month = checkMonth(month);
+     day = checkDay(day);
+     document.getElementById('data').innerHTML = day + "-" + month + "-" + year;
+}, false);
+   
+function checkMonth(j){
+     if (j<10) { 
+          j= "0" + j;
+     }
+     return j;
+};
+
+function checkDay(k)
+{
+     if(k<10) {
+          k = "0" + k;
+         
+     }
+     return k;
+
+
+}   
+window.addEventListener("load", () => {
+    
+      let currYear = currentYear();
+      this.document.getElementById('year').innerHTML = currYear;
+      
+},false);
+   
+   
+function currentYear() {
+     let currentYear = new Date();
+     let year = currentYear.getFullYear();
+   
+     return year;
+ };
+   
+   
+const todayYear = currentYear();
+   
+const previousYear = document.getElementById('year-previous');
+   
+let preYear =  todayYear;
+         
+let savedYear = 0; 
+   
+let yearIndex = 0;
+   
+previousYear.addEventListener("click", () => { 
+     preYear = preYear -1;   
+     document.getElementById('year').innerHTML= preYear;
+     yearIndex++;
+     savedYear = preYear;     
+   });
+   
+   
+
+const nextYear = document.getElementById('year-next');
+   
+nextYear.addEventListener("click", () => {
+       if(yearIndex <= 1)
+               {savedYear++; }
+       else {savedYear = savedYear + yearIndex;}
+          
+   
+        document.getElementById('year').innerHTML= savedYear;
+   
+}) ;
