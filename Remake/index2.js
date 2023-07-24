63fe9d11936b90350cf6b12b67e5ac7789e320e5
Yunrai <darksh13@gmail.com>
2023-07-24 15:53:16 +0300

    Made the booking calendar fully functional + additional redesigns

--- Remake/index2.js
+++ Remake/index2.js
@@ -24,7 +24,7 @@ window.addEventListener('resize', () => {
 
  //booking calendar
 
-window.addEventListener("load", function startTime() {
+function startTime() {
      const today = new Date();
      let h = today.getHours();
      let m = today.getMinutes();
@@ -32,15 +32,15 @@ window.addEventListener("load", function startTime() {
      m = checkTime(m);
      s = checkTime(s);
      document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
-     setTimeout(startTime, 1000);
-}, false);
+     setTimeout(startTime, 1000); 
+}
    
 function checkTime(i) {
      if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
      return i;
 };
    
-window.addEventListener("load", function currentDate(){
+function currentDate(){
      const today = new Date();
      let day = today.getDate();
      let month = today.getMonth() + 1;
@@ -48,8 +48,8 @@ window.addEventListener("load", function currentDate(){
      month = checkMonth(month);
      day = checkDay(day);
      document.getElementById('data').innerHTML = day + "-" + month + "-" + year;
-}, false);
-   
+};
+
 function checkMonth(j){
      if (j<10) { 
           j= "0" + j;
@@ -64,8 +64,6 @@ function checkDay(k)
          
      }
      return k;
-
-
 }   
 
 //Month Pick
@@ -74,18 +72,74 @@ let date = new Date();
 let currYear = date.getFullYear();
 let currMonth = date.getMonth();
 let currentMonth = document.querySelector('.month-pick');
+
 console.log(currMonth);
 
 const months = ["Ianuarie","Februarie","Martie","Aprilie","Mai","Iunie","Iulie","August","Septembrie","Octombrie","Noiembrie","Decembrie"];
 
 const days = document.querySelector(".calendar-days");
 
-window.addEventListener("load", () =>{
-     this.document.getElementById('month-pick').innerHTML = months[currMonth];
-}, false);
+const changeMonth = document.querySelectorAll(".year-change pre");
 
- //Getting last day of month
+const preMonth = document.querySelector("#year-previous");
+const nextMonth = document.querySelector("#year-next");
 
+
+const renderCalendar = () =>{
+     startTime();
+     currentDate();
+     currentMonth.innerHTML = months[currMonth];
+     
+      //Getting last day of month
+     let firstDateofMonth = new Date(currYear, currMonth, 1).getDay(); //get the first day of month
+     let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();//get the last date of month
+     let lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); //get the last date of previous month
+     let lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(); //get the last day of month
+     
+     console.log(lastDateofMonth);
+
+     let daysList = "";
+
+     for (let i=firstDateofMonth; i > 0; i--) {
+          daysList += `<div class="inactive">${lastDateofLastMonth-i+1}</div>`;
+
+     }
+
+     for (let i = 1; i <= lastDateofMonth; i++) { // creating li of all days of current month
+          // adding active class to li if the current day, month, and year matched
+          let isToday = i === date.getDate() && currMonth === new Date().getMonth() 
+                       && currYear === new Date().getFullYear() ? "active" : "";
+          daysList += `<div class="${isToday}">${i}</div>`;
+    }
+
+     for(let i=lastDateofMonth; i<6 ; i++){
+          daysList += `<div class="inactive">${i-lastDateofLastMonth+1}</div>`;
+     }
+          
+     days.innerHTML = daysList;     
+          
+     //Show Current Year  
+          
+      this.document.getElementById('year').innerHTML = currYear;
+     
+};
+renderCalendar();
+
+changeMonth.forEach(pre => {
+     pre.addEventListener("click", () => {
+          currMonth = pre.id === "year-previous" ? currMonth - 1 : currMonth + 1;
+          if(currMonth < 0 || currMonth > 11){   //update the current year and month
+               date = new Date(currYear, currMonth);
+               currYear = date.getFullYear();
+               currMonth = date.getMonth();
+          } else
+          {
+               date = new Date();
+          }
+          renderCalendar();
+     })
+ })
+/*
  window.addEventListener("load", () => {
      let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
      console.log(lastDateofMonth);
@@ -96,7 +150,7 @@ window.addEventListener("load", () =>{
           days.innerHTML = daysList;               
 },false);
 
- let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
+/*let lastDateofMonth = new Date(currYear, currMonth + 1, 0).getDate();
 console.log(lastDateofMonth);
 let daysList = "";
 for(let i=1; i<=lastDateofMonth; i++ ){
@@ -113,13 +167,13 @@ window.addEventListener("load", () => {
 },false);
    
    
-function currentYear() {
-     let currentYear = new Date();
-     let year = currentYear.getFullYear();
+
    
-     return year;
- };
-   
+//previous Year
+
+
+
+ /*
    
 const todayYear = currentYear();
    
@@ -150,4 +204,4 @@ nextYear.addEventListener("click", () => {
    
         document.getElementById('year').innerHTML= savedYear;
    
-}) 
+}) */
